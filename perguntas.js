b8c133fe5bfc0525cc4e20d443937d2b0dede6ed
Enzo Andrade <168945602+enzinho06@users.noreply.github.com>
2024-10-07 14:58:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia',
+    'O que é Alura?',
+    'A Alura é uma escola online de tecnologia brasileira que oferece cursos, fórum e outras ferramentas para estudar e aprender sobre tecnologia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma div?',
+    'Div é uma tag do HTML que serve para agrupar blocos de elementos e aplicar estilos CSS a eles.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Mais em Inglês?',
+    'Mais em ingles é MORE '
+)
